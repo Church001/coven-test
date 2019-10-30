1c6408f76de2023b03ba0197078b5b0881cc3ddc
church <simonreuben23@gmail.com>
2019-10-30 06:03:28 +0100

    m

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,20 +6,63 @@ import Fade from '@material-ui/core/Fade';
 import { connect } from 'react-redux';
 import Button from '@material-ui/core/Button';
 import { clearAirport } from '../redux/actions/action';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import { Grid } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   modal: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+
+  },
+  card: {
+    maxWidth: 345,
+    height:"50%"
   },
   paper: {
     backgroundColor: theme.palette.background.paper,
     border: '2px solid #000',
     boxShadow: theme.shadows[5],
     width: '60%',
-    height: '50%',
-    padding: theme.spacing(2, 4, 3),
+    maxHeight: '50%',
+    padding: theme.spacing(2, 1, 3),
+  },
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+    height:"600"
+  },
+  gridList: {
+    width: '50%',
+    height: '100%',
+  },
+  submit: {
+    margin: theme.spacing(0, 0, 2),
+    width: '250'
+  },
+  button: {
+    float: 'right'
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    // marginRight: theme.spacing(1),
+    width: 250,
   },
 }));
 
@@ -53,17 +96,75 @@ const MyModal = (props) => {
         }}
       >
         <Fade in={open}>
-          <div className={classes.paper}>
-            <h2 id="transition-modal-title">{open ? airport.city: ""}</h2>
-            <p id="transition-modal-description">react-transition-group animates me.</p>
+          <Grid className={classes.paper}>
             <Button 
-                variant="contained" 
-                color="primary" 
-                onClick={() => handleClose()}
-            >
-                Close
+              variant="contained" 
+              color="secondary" 
+              onClick={() => handleClose()}
+              className={classes.button}
+              >
+              Close
             </Button>
-          </div>
+            <h2 id="transition-modal-title">{open ? airport.city: ""}</h2>
+            {/* <p id="transition-modal-description">react-transition-group animates me.</p> */}
+            <div className={classes.root}>
+             <div>
+               
+              <div>
+                <TextField
+                  id="filled-number"
+                  label="Number"
+                  type="number"
+                  className={classes.textField}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  margin="normal"
+                  variant="filled"
+                />
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                    size='small'
+                    // onClick={submission}
+                >
+                    Fetch
+                </Button>
+              </div>
+              <div>
+                <TextField
+                  id="filled-number"
+                  label="Number"
+                  type="number"
+                  className={classes.textField}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  margin="normal"
+                  variant="filled"
+                />
+                 <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                    // onClick={submission}
+                >
+                    Fetch
+                </Button>
+              </div>             
+             </div>
+              <GridList cellHeight={160} className={classes.gridList} cols={1}>
+                  <GridListTile >
+                    <h1>HEAD</h1>
+                  </GridListTile>
+              </GridList>
+            </div>
+          </Grid>
         </Fade>
       </Modal>
     </div>
